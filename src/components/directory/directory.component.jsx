8be0812642a548b2bd9dd6829711ed986cc6ba14
francianepovoa <francianerpovoa@gmail.com>
2020-09-21 16:44:00 +0200

    broken homepage into smaller components, finished styling our menu-item components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import MenuItem from "../menu-item/menu-item.component";
+
 import "./directory.styles.scss";
 
 class Directory extends React.Component {
@@ -13,33 +14,28 @@ class Directory extends React.Component {
 					title: "hats",
 					imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
 					id: 1,
-					linkUrl: "shop/hats",
 				},
 				{
 					title: "jackets",
 					imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
 					id: 2,
-					linkUrl: "shop/jackets",
 				},
 				{
 					title: "sneakers",
 					imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
 					id: 3,
-					linkUrl: "shop/sneakers",
 				},
 				{
 					title: "women",
 					imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
 					size: "large",
 					id: 4,
-					linkUrl: "shop/women",
 				},
 				{
 					title: "mens",
 					imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
 					size: "large",
 					id: 5,
-					linkUrl: "shop/mens",
 				},
 			],
 		};
