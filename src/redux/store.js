d0261e728354ca992d943300432f66ddeb52c3bc
francianepovoa <francianerpovoa@gmail.com>
2020-09-30 16:03:13 +0200

    Setting up Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 
 import logger from "redux-logger";
 
-import rootReducer from "./root-reducer";
+import rootReducer from "./user/user.reducer";
 
 const middlewares = [logger];
 
