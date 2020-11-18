30c71f9e1d2d8ade5a7bce2f183a50a4b21bd09d
francianepovoa <francianerpovoa@gmail.com>
2020-11-18 16:17:14 +0100

    Redux 2: Directory, Collection State into Redux. Collection overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,11 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-class ShopPage extends React.Component {
-	constructor(props) {
-		super(props);
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-		this.state = {
-			collections: SHOP_DATA,
-		};
-	}
-
-	render() {
-		const { collections } = this.state;
-		return (
-			<div className='shop-page'>
-				{collections.map(({ id, ...otherCollectionsProps }) => (
-					<CollectionPreview key={id} {...otherCollectionsProps} />
-				))}
-			</div>
-		);
-	}
-}
+const ShopPage = ({ collections }) => (
+	<div className='shop-page'>
+		<CollectionsOverview />
+	</div>
+);
 
 export default ShopPage;
