d7b1c5a651b46c826892cbfe1897ccceabb40566
francianepovoa <francianerpovoa@gmail.com>
2020-11-19 15:30:47 +0100

    Nested Routing Shop page, Collection Routing and Selector.

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
+import "./collection.component";
+
+const CollectionPage = ({ match }) => {
+	console.log(match);
+	return (
+		<div className='collection-page'>
+			<h2>COLLECTION PAGE</h2>
+		</div>
+	);
+};
+
+const mapStateToProps = (state, ownProps) => ({
+	collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
