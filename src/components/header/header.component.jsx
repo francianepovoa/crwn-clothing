30c1fdd855107b6a64e2c8832a470b01524d4830
francianepovoa <francianerpovoa@gmail.com>
2020-11-30 16:01:06 +0100

    CSS in JS - styled-components, homepage and headercomponent

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -12,33 +11,31 @@ import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import "./header.styles.scss";
+import {
+	HeaderContainer,
+	LogoContainer,
+	OptionContainer,
+	OptionDiv,
+	OptionLink,
+} from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
-	<div className='header'>
-		<Link className='logo-container' to='/'>
+	<HeaderContainer>
+		<LogoContainer to='/'>
 			<Logo className='logo' />
-		</Link>
-		<div className='options'>
-			<Link className='option' to='/shop'>
-				SHOP
-			</Link>
-			<Link className='option' to='/shop'>
-				CONTACT
-			</Link>
+		</LogoContainer>
+		<OptionContainer>
+			<OptionLink to='/shop'>SHOP</OptionLink>
+			<OptionLink to='/shop'>CONTACT</OptionLink>
 			{currentUser ? (
-				<div className='option' onClick={() => auth.signOut()}>
-					SIGN OUT
-				</div>
+				<OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
 			) : (
-				<Link className='option' to='/signin'>
-					SIGN IN
-				</Link>
+				<OptionLink to='/signin'>SIGN IN</OptionLink>
 			)}
 			<CartIcon />
-		</div>
+		</OptionContainer>
 		{hidden ? null : <CartDropdown />}
-	</div>
+	</HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Directory from "../../components/directory/directory.component";
 
-// import sass file
-import "./homepage.styles.scss";
+import { HomePageContainer } from "./homepage.styles";
 
 const HomePage = () => (
-	<div className='homepage'>
+	<HomePageContainer>
 		<Directory />
-	</div>
+	</HomePageContainer>
 );
 
 export default HomePage;
