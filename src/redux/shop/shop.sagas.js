c1926530979241a45fa2ba68d06a4f2728dbfef1
francianepovoa <francianerpovoa@gmail.com>
2021-02-17 17:07:12 +0100

    React Hooks:Use Effects in aour App/Collection

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -10,7 +10,6 @@ import {
 } from "./shop.actions";
 
 export function* fetchCollectionsAsync() {
-	yield console.log("I am fired");
 	try {
 		const collectionRef = firestore.collection("collections");
 		const snapshot = yield collectionRef.get();
